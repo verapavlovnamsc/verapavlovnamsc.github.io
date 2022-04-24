f3fccba67804e2518cbedf299a7f72b701f3f7db
verapavlovnamsc <40237715+verapavlovnamsc@users.noreply.github.com>
2022-04-24 07:01:40 +0300

    Add files via upload

---
+++ partials/footer.js
@@ -0,0 +1,20 @@
+document.write(      
+    `<footer>     
+        <div class="footercontent">       
+            <!-- <div class="arrow-top"></div>   -->
+            <button onclick="topFunction()" id="myBtn"  title="Go to top">Наверх</button>          
+            <div class="niz">
+                <ul>
+                    <li><a href="collagen.html">Коллаген</a></li>
+                    <li><a href="kigelia.html">Кигелия</a></li>
+                    <li><a href="skin.html">Влияние на кожу</a></li>
+                    <li><a href="wheretobuy.html">Где купить?</a></li>
+                </ul>
+            </div>
+            <div class="niz">
+            Москва, <br>
+            Научный проезд, 5                
+            </div>                       
+        </div>         
+   </footer>`
+);
